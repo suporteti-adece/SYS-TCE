e6efcaa7751dacb0a41d7b40f247b33ef1b7b159
TalysonSoares <talysonsoares40@gmail.com>
2025-09-17 15:43:26 -0300

    Create Controller

---
+++ src/Controller/Web/ExigibilidadeWebController.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Web;
+
+use App\Service\Interface\ExigibilidadeServiceInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
+
+class ExigibilidadeWebController extends AbstractController
+{
+    private const string LIST = 'exigibilidade/list.html.twig';
+    private const string CREATE = 'exigibilidade/create.html.twig';
+    public function __construct(
+        private readonly ExigibilidadeServiceInterface $service,
+    ) {
+    }
+
+    public function list(): Response
+    {
+        $exigibilidades = $this->service->list();
+
+        return $this->render(self::LIST, [
+            'exigibilidades' => $exigibilidades
+        ]);
+    }
+
+    public function create(Request $request): Response
+    {
+        if (false === $request->isMethod(Request::METHOD_POST)) {
+            return $this->render(self::CREATE);
+        }
+
+        $errors = [];
+
+        try {
+            $this->service->create([
+                'id' => Uuid::v4(),
+                'exercicio' => $request->get('exercicio'),
+                'semestre' => $request->get('semestre'),
+                'codOrgao' => $request->get('codOrgao'),
+                'codFonteRecurso' => $request->get('codFonteRecurso'),
+                'tipoRecurso' => $request->get('tipoRecurso'),
+                'numContratoEmprestimo' => $request->get('numContratoEmprestimo') ?? null,
+                'anoContratoEmprestimo' => $request->get('anoContratoEmprestimo'),
+                'numConvenio' => $request->get('numConvenio') ?? null,
+                'anoConvenio' => $request->get('anoConvenio'),
+                'numNotaFiscal' => $request->get('numNotaFiscal'),
+                'dataNotaFiscal' => $request->get('dataNotaFiscal'),
+                'dataAtesto' => $request->get('dataAtesto'),
+                'idPagamento' => $request->get('idPagamento'),
+                'dataPagamento' => $request->get('dataPagamento'),
+                'valorPagamento' => $request->get('valorPagamento'),
+                'numContrato' => $request->get('numContrato'),
+                'anoContrato' => $request->get('anoContrato'),
+                'valorContratacao' => $request->get('valorContratacao'),
+                'cpfCnpjCredor' => $request->get('cpfCnpjCredor'),
+                'tipoExigibilidade' => $request->get('tipoExigibilidade'),
+                'justificativa' => $request->get('justificativa') ?? null,
+            ]);
+
+            $this->addFlash('success', 'Exibilidade cadastrada com sucesso!');
+        } catch (Exception $exception) {
+            $errors = [$exception->getMessage()];
+        }
+
+        if (false === empty($errors)) {
+            return $this->render(self::CREATE, [
+                'errors' => $errors,
+            ]);
+        }
+
+        return $this->redirectToRoute('exigibilidade_list');
+    }
+}
--- src/Service/Interface/ExigibilidadeServiceInterface.php
+++ src/Service/Interface/ExigibilidadeServiceInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Service\Interface;
 
+use App\Entity\Exigibilidade;
+
 interface ExigibilidadeServiceInterface
 {
+    public function create(array $data): Exigibilidade;
 
+    public function list(int $limit = 100, array $params = []): array;
 }
