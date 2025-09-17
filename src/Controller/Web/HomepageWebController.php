616e7a55b6eaced92f7538243eb9f2857d4084d6
TalysonSoares <talysonsoares40@gmail.com>
2025-09-17 10:41:28 -0300

    First commit

---
+++ src/Controller/Web/HomepageWebController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomepageWebController extends AbstractController
+{
+    public function homepage(): Response
+    {
+        return $this->render("home.html.twig");
+    }
+}
